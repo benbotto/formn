acd9b67d7760285e60de7ae93b6078c7aee8a42a
Ben Botto <benjamin.botto@gmail.com>
2018-11-24 15:01:38 -0800

    ForeignKey moved to ForeignKeyMetadata.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import 'reflect-metadata';
 
 import { User } from './test/entity/user.entity';
+import { PhoneNumber } from './test/entity/phone-number.entity';
 import metaFactory from './database/metadata-factory';
 
 const user = new User();
+const pn = new PhoneNumber();
 //console.log(user);
 
 //console.log(new metaFactory.getColumnMetadata()[0].entity);
 //const Entity: {new(): any} = metaFactory.getDatabaseMetaData().getTables()[0].Entity;
 //console.log(metaFactory.getDatabaseMetaData().getTables()[0].produceEntity());
 //console.log(new Entity());
-console.log(metaFactory.getDatabaseMetaData().getTables()[0].getColumns());
+//console.log(metaFactory.getDatabaseMetaData().getTables()[0].getColumns());
+//console.log(metaFactory.getDatabaseMetaData().getTables()[1].getColumns());
+console.log(metaFactory.getForeignKeyMetadata()[0].getRefEntity());
 

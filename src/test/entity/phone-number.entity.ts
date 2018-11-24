3b3a3f9df8d5fea167daea23277e3178b54aaaef
Ben Botto <benjamin.botto@gmail.com>
2018-11-24 14:59:47 -0800

    Adds a PhoneNumber entity and relates it to User.

---
+++ src/test/entity/phone-number.entity.ts
@@ -0,0 +1,20 @@
+import { Table } from '../../database/table.decorator';
+import { Column } from '../../database/column.decorator';
+import { ForeignKey } from '../../database/foreign-key.decorator';
+import { User } from './user.entity';
+
+@Table({name: 'phone_numbers'})
+export class PhoneNumber {
+  @Column({name: 'phoneNumberID', isPrimary: true, isGenerated: true})
+  id: number;
+
+  @Column()
+  phoneNumber: string;
+
+  @Column()
+  type: string;
+
+  @ForeignKey({column: 'userID', getReferencedTable: () => User})
+  user: User;
+}
+
--- src/test/entity/user.entity.ts
+++ src/test/entity/user.entity.ts
@@ -1,15 +1,24 @@
 import { Table } from '../../database/table.decorator';
 import { Column } from '../../database/column.decorator';
+import { PhoneNumber } from './phone-number.entity';
 
 @Table({name: 'users'})
 export class User {
-  @Column({isPrimary: true, isGenerated: true})
+  @Column({name: 'userID', isPrimary: true, isGenerated: true})
   id: number;
 
   @Column({maxLength: 255})
   username: string;
 
+  @Column({name: 'firstName', maxLength: 255})
+  first: string;
+
+  @Column({name: 'lastName', maxLength: 255})
+  last: string;
+
   @Column()
   createdOn: Date;
+
+  phoneNumbers: PhoneNumber[];
 }
 

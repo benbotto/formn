bd0a5f4243f16fdc2e68dabd14f753f720928194
Ben Botto <benjamin.botto@gmail.com>
2018-11-24 13:34:36 -0800

    Column metadata complete with tests.

---
+++ src/database/column-metadata.spec.ts
@@ -0,0 +1,74 @@
+import 'jasmine';
+
+import { ColumnMetadata } from './column-metadata';
+import { Converter } from '../converter/converter';
+
+import { User } from '../test/entity/user.entity';
+
+describe('Column()', () => {
+  /**
+   * Constructor.
+   */
+  describe('.constructor()', () => {
+    it('stores the Entity, name, and mapTo properties.', function() {
+      const col = new ColumnMetadata(User, 'username', {name: 'username'});
+      expect(col.name).toBe('username');
+      expect(col.mapTo).toBe('username');
+      expect(col.Entity).toBe(User);
+    });
+
+    it('defaults isPrimary to false.', () => {
+      const col = new ColumnMetadata(User, 'username', {name: 'username'});
+      expect(col.isPrimary).toBe(false);
+    });
+
+    it('stores the isPrimary flag.', () => {
+      const col = new ColumnMetadata(User, 'username', {name: 'username', isPrimary: true});
+      expect(col.isPrimary).toBe(true);
+    });
+
+    it('defaults isGenerated to false.', () => {
+      const col = new ColumnMetadata(User, 'username', {name: 'username'});
+      expect(col.isGenerated).toBe(false);
+    });
+
+    it('stores the isGenerated flag.', () => {
+      const col = new ColumnMetadata(User, 'username', {name: 'username', isGenerated: true});
+      expect(col.isGenerated).toBe(true);
+    });
+
+    it('defaults isNullable to true.', () => {
+      const col = new ColumnMetadata(User, 'username', {name: 'username'});
+      expect(col.isNullable).toBe(true);
+    });
+
+    it('stores the isNullable flag.', () => {
+      const col = new ColumnMetadata(User, 'username', {name: 'username', isNullable: false});
+      expect(col.isNullable).toBe(false);
+    });
+
+    it('stores the dataType property.', () => {
+      const col = new ColumnMetadata(User, 'username', {name: 'username', dataType: 'VARCHAR'});
+      expect(col.dataType).toBe('VARCHAR');
+    });
+
+    it('stores the maxLength property.', () => {
+      const col = new ColumnMetadata(User, 'username', {name: 'username', maxLength: 100});
+      expect(col.maxLength).toBe(100);
+    });
+
+    it('stores the converter.', () => {
+      const conv = new Converter();
+      const col = new ColumnMetadata(User, 'username', {name: 'username', converter: conv});
+
+      expect(col.converter).toBe(conv);
+    });
+  });
+
+  describe('.createFQColName()', () => {
+    it('returns the unescaped name.', function() {
+      expect(ColumnMetadata.createFQColName('users', 'firstName')).toBe('users.firstName');
+    });
+  });
+});
+
--- src/database/column-metadata.ts
+++ src/database/column-metadata.ts
@@ -1,27 +1,54 @@
 import { ColumnMetaOptions } from './column-meta-options';
+import { Converter } from '../converter/converter';
 
 export class ColumnMetadata {
   Entity: {new(): any};
   mapTo: string;
   name: string;
-  primary: boolean;
-  generated:boolean;
+  isPrimary: boolean;
+  isGenerated:boolean;
   defaultValue: string;
-  nullable: boolean;
+  isNullable: boolean;
   dataType: string;
   maxLength?: number;
+  converter?: Converter;
 
-  constructor(Entity: {new(): any}, mapTo: string, options: ColumnMetaOptions) {
+  /**
+   * Initialize the metadata for a column.
+   * @param {class} Entity - The constructor for the class to which this column
+   * will be mapped.
+   * @param {string} mapTo - The name of the property in the class to which this
+   * column will be mapped.
+   * @param {ColumnMetaOptions} - Configuration options for this column with
+   * metadata like dataType, column name, etc.
+   */
+  constructor(
+    Entity: {new(): any},
+    mapTo: string,
+    options: ColumnMetaOptions) {
+
     this.Entity       = Entity;
     this.mapTo        = mapTo;
 
     this.name         = options.name;
-    this.primary      = options.primary || false;
-    this.generated    = options.generated || false;
+    this.isPrimary    = options.isPrimary || false;
+    this.isGenerated  = options.isGenerated || false;
     this.defaultValue = options.defaultValue || null;
-    this.nullable     = options.nullable || true;
     this.dataType     = options.dataType;
     this.maxLength    = options.maxLength;
+    this.isNullable   = options.isNullable === undefined ? true : options.isNullable;
+    this.converter    = options.converter;
+  }
+
+  /**
+   * Create a fully-qualified column name in the form
+   * &lt;table-alias&gt;.&lt;column-name&gt;.
+   * @param {string} tableAlias - The alias for the table.
+   * @param {string} colName - The column name.
+   * @return {string} The fully-qualified column name, unescaped.
+   */
+  static createFQColName(tableAlias: string, colName: string): string {
+    return `${tableAlias}.${colName}`;
   }
 }
 

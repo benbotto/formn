6020c28ab1325bb189f94bdc57877ba59cbe7d88
Ben Botto <benjamin.botto@gmail.com>
2018-11-29 15:37:38 -0800

    Removes foreign keys (replaced with Relationship).

--- src/database/foreign-key-meta-options.ts
+++
@@ -1,9 +0,0 @@
-export class ForeignKeyMetaOptions {
-  // The name of the column in this table that references the foreign table.
-  column: string;
-
-  // Function that returns the type of Table that this references (e.g.
-  // the constructor of the associated Table-decorated class).
-  getReferencedTable: () => {new(): any};
-}
-

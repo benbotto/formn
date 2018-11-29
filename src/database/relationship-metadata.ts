c97b39f9fe287b6fc4bf7dc180d8c55ebb88270f
Ben Botto <benjamin.botto@gmail.com>
2018-11-29 15:38:44 -0800

    Metadata for relationships.

---
+++ src/database/relationship-metadata.ts
@@ -0,0 +1,27 @@
+import { CardinalityType } from './cardinality-type';
+
+/** Represents a relationship between two tables. */
+export class RelationshipMetadata {
+  /**
+   * Initialize the relationship metadata.
+   * @param Entity - The type (constructor) of the table where this
+   * relationship was defined.
+   * @param mapTo - The property in Entity on which this relationship was
+   * defined.
+   * @param to - A function that, when called, returns the referenced
+   * Entity type (constructor).
+   * @param on - A function that, when called, returns an array consisting of
+   * the names of the local and remote properties (e.g. [user.id,
+   * phoneNumber.userID]).  The function will be passed property maps
+   * containing all the decorated properties of Entity and RefEntity.
+   * @param cardinality - The relationship type (e.g. OneToMany).
+   */
+  constructor(
+    public Entity: {new(): any},
+    public mapTo: string,
+    public to: () => {new(): any},
+    public on: (entity: any, refEntity: any) => string[],
+    public cardinality: CardinalityType) {
+  }
+}
+

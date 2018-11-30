14afa07d7bb038950d4acbf1ac3ee4a0389c40c0
Ben Botto <benjamin.botto@gmail.com>
2018-11-30 09:23:20 -0800

    Adds the TableStore with a getter.

--- src/metadata/metadata-factory.ts
+++ src/metadata/metadata-factory.ts
@@ -1,9 +1,11 @@
 import { DatabaseMetaData } from './database/database-metadata';
+import { TableStore } from './table/table-store';
 import { ColumnStore } from './column/column-store';
 import { RelationshipStore } from './relationship/relationship-store';
 
 class MetadataFactory {
   private dbMetaMap: Map<string, DatabaseMetaData> = new Map();
+  private tblStore: TableStore = new TableStore();
   private colStore: ColumnStore = new ColumnStore();
   private relStore: RelationshipStore = new RelationshipStore();
 
@@ -13,6 +15,7 @@ class MetadataFactory {
    */
   clear(): MetadataFactory {
     this.dbMetaMap = new Map();
+    this.tblStore = new TableStore();
     this.colStore = new ColumnStore();
     this.relStore = new RelationshipStore();
 
@@ -30,6 +33,13 @@ class MetadataFactory {
       this.dbMetaMap.set(name, new DatabaseMetaData(name));
 
     return this.dbMetaMap.get(name);
+  }
+
+  /**
+   * Get the global TableStore instance.
+   */
+  getTableStore(): TableStore {
+    return this.tblStore;
   }
 
   /**

4b81c474fc7bf87d36d89b0852944947cea1bbea
Ben Botto <benjamin.botto@gmail.com>
2018-11-30 14:45:04 -0800

    CHORE: Property cleanup.

--- src/metadata/table/table-metadata.ts
+++ src/metadata/table/table-metadata.ts
@@ -2,20 +2,16 @@ import { TableMetaOptions } from './table-meta-options';
 import { EntityType } from './entity-type';
 
 export class TableMetadata {
-  Entity: EntityType;
-  name: string;
-  database: string;
-
   /**
    * Initialize the Table's metadata.
    * @param Entity - The constructor for the Table-decorated class.
    * @param name - The name of the database table.
    * @param database - The database to which this table belongs.
    */
-  constructor(Entity: EntityType, name: string, database: string) {
-    this.Entity   = Entity;
-    this.name     = name;
-    this.database = database;
+  constructor(
+    public Entity: EntityType,
+    public name: string,
+    public database: string) {
   }
 
   /**

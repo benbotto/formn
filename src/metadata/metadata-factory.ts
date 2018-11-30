b2bc7f75c3a80dacbd87e84b24c5c071939e5562
Ben Botto <benjamin.botto@gmail.com>
2018-11-30 09:29:32 -0800

    Removes DatabaseMetadata (unused).

--- src/metadata/metadata-factory.ts
+++ src/metadata/metadata-factory.ts
@@ -1,10 +1,8 @@
-import { DatabaseMetaData } from './database/database-metadata';
 import { TableStore } from './table/table-store';
 import { ColumnStore } from './column/column-store';
 import { RelationshipStore } from './relationship/relationship-store';
 
 class MetadataFactory {
-  private dbMetaMap: Map<string, DatabaseMetaData> = new Map();
   private tblStore: TableStore = new TableStore();
   private colStore: ColumnStore = new ColumnStore();
   private relStore: RelationshipStore = new RelationshipStore();
@@ -14,25 +12,11 @@ class MetadataFactory {
    * @return {this}
    */
   clear(): MetadataFactory {
-    this.dbMetaMap = new Map();
     this.tblStore = new TableStore();
     this.colStore = new ColumnStore();
     this.relStore = new RelationshipStore();
 
     return this;
-  }
-
-  /**
-   * Get the DatabaseMetaData for a database by name.
-   * @param [name='default'] - The name of the database, or 'default'
-   * if not provided.
-   * @return {DatabaseMetaData}
-   */
-  getDatabaseMetaData(name: string = 'default'): DatabaseMetaData {
-    if (!this.dbMetaMap.has(name))
-      this.dbMetaMap.set(name, new DatabaseMetaData(name));
-
-    return this.dbMetaMap.get(name);
   }
 
   /**

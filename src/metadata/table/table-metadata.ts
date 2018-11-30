158342e6b89c74742c11b0dc1da4e6102766887e
Ben Botto <benjamin.botto@gmail.com>
2018-11-29 18:48:26 -0800

    Column lookup functions moved to ColumnStore.

--- src/metadata/table/table-metadata.ts
+++ src/metadata/table/table-metadata.ts
@@ -1,25 +1,16 @@
 import { TableMetaOptions } from './table-meta-options';
 import { ColumnMetadata } from '../column/column-metadata';
-import metaFactory from '../metadata-factory';
 
 export class TableMetadata {
   Entity: {new (): any};
   name: string;
   mapTo: string;
   database: string;
-  columns: ColumnMetadata[] = [];
 
   constructor(Entity: {new (): any}, options: TableMetaOptions) {
     this.Entity   = Entity;
     this.name     = options.name;
     this.database = options.database;
-
-    const colMeta = metaFactory.getColumnMetadata();
-
-    for (let col of colMeta) {
-      if (col.Entity === this.Entity)
-        this.columns.push(col);
-    }
   }
 
   /**
@@ -28,14 +19,6 @@ export class TableMetadata {
    */
   produceEntity(): any {
     return new this.Entity();
-  }
-
-  /**
-   * Get the list of columns for this table.
-   * @return {ColumnMetadata[]} An array of all column metadata for this table.
-   */
-  getColumns(): ColumnMetadata[] {
-    return this.columns;
   }
 }
 

a6c1898f9e0c730a0e6cb11e7b9929eefc1d05d7
Ben Botto <benjamin.botto@gmail.com>
2018-12-04 14:12:44 -0800

    Error class specifically for condition compling.

---
+++ src/error/condition-error.spec.ts
@@ -0,0 +1,14 @@
+import { ConditionError } from './condition-error';
+
+describe('ConditionError()', function() {
+  describe('.constructor()', function() {
+    it('has the correct message, name, and detail.', function() {
+      const ce = new ConditionError('An error.');
+
+      expect(ce.name).toBe('ConditionError');
+      expect(ce.message).toBe('An error.');
+      expect(ce.detail).toBe('An error.');
+    });
+  });
+});
+
---
+++ src/error/condition-error.ts
@@ -0,0 +1,22 @@
+/** 
+ * Custom Error instance for Condition (lex/parse/compile) errors.
+ */
+export class ConditionError extends Error {
+  public name: string;
+  public detail: string;
+
+  /**
+   * Create the Error instance with a user-supplied message.
+   * @param {string} message - The Description of the error.
+   */
+  constructor(message: string) {
+    super(message);
+
+    // Design limitation in TypeScript when extending built-in types.
+    // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#generated-constructor-code-substitutes-the-return-value-of-super-calls-as-this
+    (<any>Object).setPrototypeOf(this, new.target.prototype);
+
+    this.name   = 'ConditionError';
+    this.detail = message;
+  }
+}

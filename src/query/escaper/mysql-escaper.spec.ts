32704a77a3a3e45b6feddedccfdb8b8b4869952c
Ben Botto <benjamin.botto@gmail.com>
2018-12-05 09:46:44 -0800

    Escaper for mysql.

---
+++ src/query/escaper/mysql-escaper.spec.ts
@@ -0,0 +1,34 @@
+import { MySQLEscaper } from './mysql-escaper';
+import { Escaper } from './escaper';
+
+describe('MySQLEscaper()', function() {
+  const escaper = new MySQLEscaper();
+
+  describe('.constructor().', function() {
+    it('extends Escaper.', function() {
+      expect(escaper instanceof Escaper).toBe(true);
+      expect(escaper instanceof MySQLEscaper).toBe(true);
+    });
+  });
+
+  describe('.escapeProperty()', function() {
+    it('escapes strings.', function() {
+      expect(escaper.escapeProperty('name')).toBe('`name`');
+    });
+
+    it('preserves dots.', function() {
+      expect(escaper.escapeProperty('my.name')).toBe('`my.name`');
+    });
+  });
+
+  describe('.escapeFullqyQualifiedColumn()', function() {
+    // Escapes a fully-qualified column.
+    it('escapes the table and the column independently.', function() {
+      expect(escaper.escapeFullyQualifiedColumn('users.firstName')).toBe('`users`.`firstName`');
+      expect(escaper.escapeFullyQualifiedColumn('users.first.Name')).toBe('`users`.`first.Name`');
+      expect(escaper.escapeFullyQualifiedColumn('phone_numbers.phoneNumber')).toBe('`phone_numbers`.`phoneNumber`');
+      expect(escaper.escapeFullyQualifiedColumn('phoneNumber')).toBe('`phoneNumber`');
+    });
+  });
+});
+
---
+++ src/query/escaper/mysql-escaper.ts
@@ -0,0 +1,24 @@
+import { Escaper } from './escaper';
+
+/**
+ * Helper class for escaping parts of a query under MySQL.
+ */
+export class MySQLEscaper extends Escaper {
+  /**
+   * Initialize the escaper.
+   */
+  constructor() {
+    super();
+  }
+
+  /**
+   * Escape a property, such as a table, column name, or alias, using
+   * backticks.
+   * @param prop - The property to escape.
+   * @return The escaped property.
+   */
+  escapeProperty(prop: string): string {
+    return `\`${prop}\``;
+  }
+}
+

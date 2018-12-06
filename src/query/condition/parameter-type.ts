67c5d1a86b18ea21cb2e24f5718abdc1ea45f753
Ben Botto <benjamin.botto@gmail.com>
2018-12-06 11:00:17 -0800

    Parameter values can be any time.

--- src/query/condition/parameter-type.ts
+++ src/query/condition/parameter-type.ts
@@ -2,4 +2,4 @@
  * Simple key-value pairs that are intended to replace parameters in a query
  * condition.
  */
-export type ParameterType = {[key: string]: string};
+export type ParameterType = {[key: string]: any};

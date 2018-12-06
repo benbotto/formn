4e0005f10fe07380f7582266e50e7d5c937b867d
Ben Botto <benjamin.botto@gmail.com>
2018-12-06 10:09:23 -0800

    Adds the join method to join in tables.  Condition derived, but needs the option of overriding conditions.

--- src/query/from/from.spec.ts
+++ src/query/from/from.spec.ts
@@ -1,5 +1,86 @@
+import metaFactory from '../../metadata/metadata-factory';
+import { TableStore } from '../../metadata/table/table-store';
+import { ColumnStore } from '../../metadata/column/column-store';
+import { RelationshipStore } from '../../metadata/relationship/relationship-store';
+import { PropertyMapStore } from '../../metadata/property/property-map-store';
+import { EntityType } from '../../metadata/table/entity-type';
+
+import { MySQLEscaper } from '../escaper/mysql-escaper';
+
+import { User } from '../../test/entity/user.entity';
+import { PhoneNumber } from '../../test/entity/phone-number.entity';
+import { Photo } from '../../test/entity/photo.entity';
+import { UserXProduct } from '../../test/entity/user-x-product.entity';
+import { Product } from '../../test/entity/product.entity';
+
+import { initDB } from '../../test/entity/database';
 import { From } from './from';
 
 describe('From()', () => {
+  let relStore: RelationshipStore;
+  let tblStore: TableStore;
+  let colStore: ColumnStore;
+  let propStore: PropertyMapStore;
+  let escaper: MySQLEscaper;
+  let getFrom: (FromEntity: EntityType, fromAlias?: string) => From;
+
+  beforeEach(function() {
+    initDB();
+
+    tblStore  = metaFactory.getTableStore();
+    colStore  = metaFactory.getColumnStore();
+    relStore  = metaFactory.getRelationshipStore();
+    propStore = metaFactory.getPropertyMapStore();
+    escaper   = new MySQLEscaper();
+
+    // From curry: produce a From instance with just an entity and alias.
+    getFrom = (FromEntity: EntityType, fromAlias?: string) =>
+      new From(colStore, tblStore, relStore, propStore, escaper, FromEntity, fromAlias);
+  });
+
+  describe('.constructor()', () => {
+    it('defaults the table alias to the table name.', () => {
+      const from = getFrom(User);
+
+      expect(from.getFromMeta().alias).toBe('users');
+    });
+
+    it('uses the supplied table alias.', () => {
+      const from = getFrom(User, 'u');
+
+      expect(from.getFromMeta().alias).toBe('u');
+    });
+  });
+
+  describe('.join()', () => {
+    it('throws an error if the parent property is not in the correct format.', () => {
+      const from = getFrom(User);
+      expect(() => from.join('INNER JOIN', PhoneNumber, 'pn', 'bad'))
+        .toThrowError('Parent property must be in the form <parent-alias>.<property>.');
+    });
+
+    it('stores metadata about the join table with a derived condition.', () => {
+      const from = getFrom(User, 'u')
+        .join('INNER JOIN', PhoneNumber, 'pn', 'u.phoneNumbers');
+
+      const meta = from.getJoinMeta()[0];
+
+      expect(meta.alias).toBe('pn');
+      expect(meta.parentAlias).toBe('u');
+      expect(meta.joinType).toBe('INNER JOIN');
+      expect(meta.cond).toEqual({$eq: {'u.id': 'pn.userID'}});
+      expect(meta.relationshipMetadata).toBe(relStore.getRelationship(User, PhoneNumber, 'phoneNumbers'));
+      expect(meta.tableMetadata).toBe(tblStore.getTable(PhoneNumber));
+    });
+
+    it('throws an error if the relationship between the two tables contains the wrong number of properties.', () => {
+      const rel = relStore.getRelationship(User, PhoneNumber, 'phoneNumbers');
+
+      rel.on = () => ['asdf'];
+      expect(() => getFrom(User, 'u')
+        .join('INNER JOIN', PhoneNumber, 'pn', 'u.phoneNumbers'))
+        .toThrowError('Relationship (on) between "User" and "PhoneNumber" must contain exactly 2 properties.');
+    });
+  });
 });
 
--- src/query/from/from.ts
+++ src/query/from/from.ts
@@ -5,11 +5,18 @@ import { PropertyMapStore } from '../../metadata/property/property-map-store';
 import { Escaper } from '../escaper/escaper';
 import { EntityType } from '../../metadata/table/entity-type';
 
+import { assert } from '../../error/assert';
+
+import { FromMeta } from './from-meta';
+import { FromTableMeta } from './from-table-meta';
+import { JoinType } from './join-type';
+
 /**
  * Represents the FROM portion of a query, with any JOINs and optionally a
  * WHERE condition.  This class is used when selecting, updating, or deleting.
  */
 export class From {
+  private fromMeta: FromMeta;
   /**
    * Initialize.
    * @param colStore - Used for accessing columns in tables.
@@ -32,6 +39,76 @@ export class From {
     private escaper: Escaper,
     FromEntity: EntityType,
     fromAlias?: string) {
+
+    this.fromMeta = new FromMeta(this.colStore, this.tblStore, this.relStore);
+
+    // Table alias defaults to the name of the table.
+    if (fromAlias === undefined)
+      fromAlias = this.tblStore.getTable(FromEntity).name;
+
+    this.fromMeta.addTable(FromEntity, fromAlias);
+  }
+
+  /**
+   * Helper method to get the meta data of the FROM table, which is the first
+   * table in [[FromMeta]].
+   * @return A meta object describing the table.
+   */
+  getFromMeta(): FromTableMeta {
+    return this.fromMeta.tableMetas.values().next().value;
+  }
+
+  /**
+   * Helper method to get the meta data of the JOIN'd in tables.
+   * @return An array of meta objects describing each table.  The meta objects
+   * are in insertion order (e.g. in the order the tables were joined in).
+   */
+  getJoinMeta(): FromTableMeta[] {
+    // All but the first table (the first table is the FROM table).
+    return Array.from(this.fromMeta.tableMetas.values()).slice(1);
+  }
+
+  /**
+   * Join in a table.
+   * @param joinType - How to join the two tables (INNER JOIN, LEFT OUTER JOIN,
+   * RIGHT OUTER JOIN).
+   * @param Entity - The [[Table]]-decorated Entity to join in.
+   * @param alias - Alias of the joined-in table.
+   * @param fqParentProperty - The fully-qualified property name on the parent
+   * Entity to which this Entity will be mapped.
+   */
+  join(joinType: JoinType, Entity: EntityType, alias: string, fqParentProperty: string): From {
+    // Get the parent alias and property name.
+    const propRE = /^(\w+)\.(\w+)$/;
+
+    assert(fqParentProperty.match(propRE),
+      'Parent property must be in the form <parent-alias>.<property>.')
+
+    const [, parAlias, parProperty] = fqParentProperty.match(propRE);
+
+    // Pull the metadata about the parent.  (Throws if not found.)
+    const parTblMeta = this.fromMeta.getTableMetadataByAlias(parAlias);
+
+    // Find the relationship between the two tables.
+    const relationship = this.relStore
+      .getRelationship(parTblMeta.Entity, Entity, parProperty);
+
+    // Create the join condition.
+    const onProps = relationship
+      .on(
+        this.propStore.getPropertyMap(parTblMeta.Entity, parAlias),
+        this.propStore.getPropertyMap(Entity, alias));
+
+    assert(onProps.length === 2,
+      `Relationship (on) between "${parTblMeta.Entity.name}" and "${Entity.name}" must contain exactly 2 properties.`);
+
+    const cond = {$eq: {[`${onProps[0]}`]: `${onProps[1]}`}};
+
+    // Store the metadata about the table.
+    this.fromMeta
+      .addTable(Entity, alias, parAlias, parProperty, joinType, cond);
+
+    return this;
   }
 }
 

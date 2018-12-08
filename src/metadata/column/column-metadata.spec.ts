8eeb72bcc8d6a3d0b9df76ef51cec2d2d541ba4a
Ben Botto <benjamin.botto@gmail.com>
2018-12-07 16:05:45 -0800

    CHORE: Removes username.

--- src/metadata/column/column-metadata.spec.ts
+++ src/metadata/column/column-metadata.spec.ts
@@ -6,60 +6,57 @@ import { Converter } from '../../converter/converter';
 import { User } from '../../test/entity/user.entity';
 
 describe('Column()', () => {
-  /**
-   * Constructor.
-   */
   describe('.constructor()', () => {
     it('stores the Entity, name, and mapTo properties.', function() {
-      const col = new ColumnMetadata(User, 'username', {name: 'username'});
-      expect(col.name).toBe('username');
-      expect(col.mapTo).toBe('username');
+      const col = new ColumnMetadata(User, 'first', {name: 'firstName'});
+      expect(col.name).toBe('firstName');
+      expect(col.mapTo).toBe('first');
       expect(col.Entity).toBe(User);
     });
 
     it('defaults isPrimary to false.', () => {
-      const col = new ColumnMetadata(User, 'username', {name: 'username'});
+      const col = new ColumnMetadata(User, 'first', {name: 'firstName'});
       expect(col.isPrimary).toBe(false);
     });
 
     it('stores the isPrimary flag.', () => {
-      const col = new ColumnMetadata(User, 'username', {name: 'username', isPrimary: true});
+      const col = new ColumnMetadata(User, 'first', {name: 'firstName', isPrimary: true});
       expect(col.isPrimary).toBe(true);
     });
 
     it('defaults isGenerated to false.', () => {
-      const col = new ColumnMetadata(User, 'username', {name: 'username'});
+      const col = new ColumnMetadata(User, 'first', {name: 'firstName'});
       expect(col.isGenerated).toBe(false);
     });
 
     it('stores the isGenerated flag.', () => {
-      const col = new ColumnMetadata(User, 'username', {name: 'username', isGenerated: true});
+      const col = new ColumnMetadata(User, 'first', {name: 'firstName', isGenerated: true});
       expect(col.isGenerated).toBe(true);
     });
 
     it('defaults isNullable to true.', () => {
-      const col = new ColumnMetadata(User, 'username', {name: 'username'});
+      const col = new ColumnMetadata(User, 'first', {name: 'firstName'});
       expect(col.isNullable).toBe(true);
     });
 
     it('stores the isNullable flag.', () => {
-      const col = new ColumnMetadata(User, 'username', {name: 'username', isNullable: false});
+      const col = new ColumnMetadata(User, 'first', {name: 'firstName', isNullable: false});
       expect(col.isNullable).toBe(false);
     });
 
     it('stores the dataType property.', () => {
-      const col = new ColumnMetadata(User, 'username', {name: 'username', dataType: 'VARCHAR'});
+      const col = new ColumnMetadata(User, 'first', {name: 'firstName', dataType: 'VARCHAR'});
       expect(col.dataType).toBe('VARCHAR');
     });
 
     it('stores the maxLength property.', () => {
-      const col = new ColumnMetadata(User, 'username', {name: 'username', maxLength: 100});
+      const col = new ColumnMetadata(User, 'first', {name: 'firstName', maxLength: 100});
       expect(col.maxLength).toBe(100);
     });
 
     it('stores the converter.', () => {
       const conv = new Converter();
-      const col = new ColumnMetadata(User, 'username', {name: 'username', converter: conv});
+      const col = new ColumnMetadata(User, 'first', {name: 'firstName', converter: conv});
 
       expect(col.converter).toBe(conv);
     });

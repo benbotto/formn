f56bc14f23b1010acb6234aa6d9401dc9a70a683
Ben Botto <benjamin.botto@gmail.com>
2018-12-07 16:52:19 -0800

    Implements Select.

---
+++ src/query/select/select.spec.ts
@@ -0,0 +1,294 @@
+import { Connection } from 'mysql2/promise';
+
+import metaFactory from '../../metadata/metadata-factory';
+
+import { initDB } from '../../test/entity/database';
+
+import { RelationshipStore } from '../../metadata/relationship/relationship-store';
+import { TableStore } from '../../metadata/table/table-store';
+import { ColumnStore } from '../../metadata/column/column-store';
+import { PropertyMapStore } from '../../metadata/property/property-map-store';
+import { MySQLEscaper } from '../escaper/mysql-escaper';
+import { MySQLExecuter } from '../executer/mysql-executer';
+import { EntityType } from '../../metadata/table/entity-type';
+
+import { From } from '../from/from';
+import { Select } from './select';
+
+import { User } from '../../test/entity/user.entity';
+import { PhoneNumber } from '../../test/entity/phone-number.entity';
+import { UserXProduct } from '../../test/entity/user-x-product.entity';
+import { Product } from '../../test/entity/product.entity';
+import { Photo } from '../../test/entity/photo.entity';
+
+describe('Select()', function() {
+  let relStore: RelationshipStore;
+  let tblStore: TableStore;
+  let colStore: ColumnStore;
+  let propStore: PropertyMapStore;
+  let escaper: MySQLEscaper;
+  let executer: MySQLExecuter;
+  let con: jasmine.SpyObj<Connection>;
+  let getFrom: (FromEntity: EntityType, fromAlias?: string) => From;
+  let getSelect: <T>(from: From) => Select<T>;
+
+  function toPlain(p: any) {
+    return JSON.parse(JSON.stringify(p));
+  }
+
+  beforeEach(function() {
+    initDB();
+
+    tblStore  = metaFactory.getTableStore();
+    colStore  = metaFactory.getColumnStore();
+    relStore  = metaFactory.getRelationshipStore();
+    propStore = metaFactory.getPropertyMapStore();
+    escaper   = new MySQLEscaper();
+    con       = jasmine.createSpyObj('con', ['query']);
+    executer  = new MySQLExecuter(con);
+
+    // From curry: produce a From instance with just an entity and alias.
+    getFrom = (FromEntity: EntityType, fromAlias?: string) =>
+      new From(colStore, tblStore, relStore, propStore, escaper, FromEntity, fromAlias);
+
+    getSelect = <T>(from: From) =>
+      new Select<T>(colStore, tblStore, relStore, propStore, escaper, executer, from);
+  });
+
+  describe('.constructor()', function() {
+    it('can be initialized using a From instance.', function() {
+      expect(() => getSelect<User>(getFrom(User, 'u')))
+        .not.toThrow();
+    });
+  });
+
+  describe('.select()', function() {
+    it('throws an error if no columns are selected.', () => {
+      const query = getSelect<User>(getFrom(User, 'u'));
+
+      expect(() => query.toString())
+        .toThrowError('No columns selected.  Call select().');
+    });
+
+    it('does not require any arguments.', function() {
+      const query = getSelect<User>(getFrom(User, 'u'))
+        .select();
+
+      expect(query.toString()).toBe(
+        'SELECT  `u`.`userID` AS `u.id`,\n'          + 
+        '        `u`.`firstName` AS `u.first`,\n'    + 
+        '        `u`.`lastName` AS `u.last`,\n'      + 
+        '        `u`.`createdOn` AS `u.createdOn`\n' + 
+        'FROM    `users` AS `u`');
+    });
+
+    it('cannot be called twice on the same query.', function() {
+      expect(function() {
+        getSelect<User>(getFrom(User, 'users'))
+          .select('users.id', 'users.first', 'users.last')
+          .select('users.id', 'users.first', 'users.last');
+      }).toThrowError('select already performed on query.');
+    });
+
+    it('throws an error if one of the selected columns is invalid.', function() {
+      expect(function() {
+        getSelect<User>(getFrom(User, 'users')).select('id'); // Should be users.id.
+      }).toThrowError('Column "id" is not available.  ' +
+        'Columns must be fully-qualified (<table-alias>.<property>).');
+    });
+
+    it('throws an error if the primary key of a table is not selected.', function() {
+      expect(function() {
+        getSelect<User>(getFrom(User, 'users')).select('users.first');
+      }).toThrowError(
+        'The primary key of every table must be selected, but the ' +
+        'primary key of table "users" (alias "users") was not selected.');
+    });
+
+    it('throws an error if the primary key of a joined table is not selected.', function() {
+      expect(function() {
+        const from = getFrom(User, 'u')
+          .innerJoin(PhoneNumber, 'pn', 'u.phoneNumbers');
+
+        getSelect<User>(from)
+          .select('u.id', 'u.first', 'pn.phoneNumber');
+      }).toThrowError(
+        'The primary key of every table must be selected, but the ' +
+        'primary key of table "phone_numbers" (alias "pn") was not selected.');
+    });
+
+    it('throws an error if the same column is selected twice.', function() {
+      expect(function() {
+        getSelect<User>(getFrom(User, 'u'))
+        .select('u.id', 'u.first', 'u.first')
+      }).toThrowError('Column "u.first" already selected.');
+    });
+  });
+
+  describe('.orderBy()', function() {
+    it('cannot be called twice on the same query.', function() {
+      expect(function() {
+        getSelect<User>(getFrom(User, 'u'))
+          .select('u.id', 'u.first', 'u.last')
+          .orderBy('u.first')
+          .orderBy('u.first');
+      }).toThrowError('orderBy already performed on query.');
+    });
+
+    it('throws an error if the orderBy column is not available.', function() {
+      expect(function() {
+        getSelect<User>(getFrom(User, 'u'))
+          .select('u.id', 'u.first', 'u.last')
+          .orderBy('bad.column');
+      }).toThrowError('"bad.column" is not available for orderBy.');
+    });
+
+    it('adds the ORDER BY clause for a single column.', function() {
+      const query = getSelect<User>(getFrom(User, 'u'))
+        .select('u.id', 'u.first', 'u.last')
+        .orderBy('u.first');
+
+      expect(query.toString()).toBe(
+        'SELECT  `u`.`userID` AS `u.id`,\n'       +
+        '        `u`.`firstName` AS `u.first`,\n' +
+        '        `u`.`lastName` AS `u.last`\n'    +
+        'FROM    `users` AS `u`\n' +
+        'ORDER BY `u`.`firstName` ASC');
+    });
+
+    it('adds the ORDER BY clause for multiple columns.', function() {
+      const query = getSelect<User>(getFrom(User, 'u'))
+        .select('u.id', 'u.first', 'u.last')
+        .orderBy('u.id', 'u.first', 'u.last');
+
+      expect(query.toString()).toBe(
+        'SELECT  `u`.`userID` AS `u.id`,\n'       +
+        '        `u`.`firstName` AS `u.first`,\n' +
+        '        `u`.`lastName` AS `u.last`\n'    +
+        'FROM    `users` AS `u`\n' +
+        'ORDER BY `u`.`userID` ASC, `u`.`firstName` ASC, `u`.`lastName` ASC');
+    });
+
+    it('can have multiple directions, ASC and DESC.', function() {
+      const query = getSelect<User>(getFrom(User, 'u'))
+        .select('u.id', 'u.first', 'u.last')
+        .orderBy(
+          {fqProperty: 'u.id', dir: 'ASC'},
+          {fqProperty: 'u.first', dir: 'ASC'},
+          {fqProperty: 'u.last', dir: 'DESC'});
+
+      expect(query.toString()).toBe(
+        'SELECT  `u`.`userID` AS `u.id`,\n'       +
+        '        `u`.`firstName` AS `u.first`,\n' +
+        '        `u`.`lastName` AS `u.last`\n'    +
+        'FROM    `users` AS `u`\n' +
+        'ORDER BY `u`.`userID` ASC, `u`.`firstName` ASC, `u`.`lastName` DESC');
+    });
+  });
+
+  describe('.execute()', function() {
+    let selectSpy: jasmine.Spy;
+
+    beforeEach(() => selectSpy = spyOn(executer, 'select'));
+
+    it('executes the query using the Executer\'s select() method.', function() {
+      selectSpy.and.returnValue(Promise.resolve([{}]));
+
+      const query = getSelect<User>(getFrom(User, 'u'))
+        .select()
+        .execute();
+
+      expect(selectSpy).toHaveBeenCalled();
+    });
+
+    it('passes the pareters to the QueryExecuter\'s select() method.', function() {
+      selectSpy.and.returnValue(Promise.resolve([{}]));
+
+      const params = {userID: 12, firstName: 'Joe'};
+      const from = getFrom(User, 'u')
+        .where({
+          $and: [
+            {$eq: {'u.id': ':userID'}},
+            {$eq: {'u.first': ':firstName'}}
+          ]
+        }, params);
+
+      const query = getSelect<User>(from)
+        .select();
+
+      query
+        .execute();
+
+      expect(selectSpy.calls.argsFor(0)[0]).toEqual(query.toString());
+      expect(selectSpy.calls.argsFor(0)[1]).toEqual(params);
+    });
+
+    describe('data mapping -', function() {
+      it('serializes a single table.', (done) => {
+        const usersRaw = require('../../test/query/users.json');
+
+        selectSpy.and.returnValue(Promise.resolve(usersRaw));
+
+        getSelect<User>(getFrom(User, 'u'))
+          .select()
+          .execute()
+          .then(users => {
+            expect(users.length).toBe(4);
+            users
+              .forEach(u => expect(u instanceof User).toBe(true));
+
+            ['Joe', 'Rand', 'Holly', 'Jenny']
+              .forEach((first, i) => expect(users[i].first).toBe(first));
+            done();
+          });
+      });
+
+      it('maps the results to a normalized object using a DataMapper instance.', function(done) {
+        const usersRaw = require('../../test/query/users-with-phone-numbers-products-and-photos.aliased.json');
+
+        selectSpy.and.returnValue(Promise.resolve(usersRaw));
+
+        const from = getFrom(User, 'u')
+          .leftOuterJoin(PhoneNumber, 'pn', 'u.phoneNumbers')
+          .leftOuterJoin(UserXProduct, 'uxp', 'u.userXProducts')
+          .leftOuterJoin(Product, 'p', 'uxp.product')
+          .leftOuterJoin(Photo, 'ph', 'p.photos',
+            {
+              $and: [
+                {$eq: {'p.id' : 'ph.prodID'}},
+                {$isnt: {'ph.largeThumbnailID': null}},
+                {$isnt: {'ph.smallThumbnailID': null}},
+              ]
+            });
+
+        const query = getSelect<User>(from)
+          .orderBy('u.id', 'pn.id', 'p.id', 'ph.id')
+          .select(
+            'u.id', 'u.first', 'u.last',
+            'pn.id', 'pn.phoneNumber',
+            'uxp.id',
+            'p.id', 'p.description',
+            'ph.id', 'ph.photoURL');
+
+        query
+          .execute()
+          .then(users => {
+            expect(toPlain(users)).toEqual(require('../../test/query/users-with-phone-numbers-products-and-photos.serialized'));
+            done();
+          });
+      });
+
+      it('propagates errors that originate in the QueryExecuter.', function(done) {
+        selectSpy.and.returnValue(Promise.reject(new Error('test')));
+        getSelect<User>(getFrom(User, 'u'))
+          .select()
+          .execute()
+          .catch(err => {
+            expect(err.message).toBe('test');
+            done();
+          });
+      });
+    });
+  });
+});
+
---
+++ src/query/select/select.ts
@@ -0,0 +1,285 @@
+import { assert } from '../../error/assert';
+
+import { ColumnStore } from '../../metadata/column/column-store';
+import { TableStore } from '../../metadata/table/table-store';
+import { RelationshipStore } from '../../metadata/relationship/relationship-store';
+import { PropertyMapStore } from '../../metadata/property/property-map-store';
+import { ColumnMetadata } from '../../metadata/column/column-metadata';
+
+import { Schema } from '../../datamapper/schema';
+import { DataMapper } from '../../datamapper/data-mapper';
+
+import { Query } from '../query';
+import { Escaper } from '../escaper/escaper';
+import { Executer } from '../executer/executer';
+import { From } from '../from/from';
+import { FromColumnMeta } from '../from/from-column-meta';
+import { OrderByType } from './order-by-type';
+import { ExecutableQuery } from '../executable-query';
+
+/**
+ * Represents a SELECT query.
+ */
+export class Select<T> extends Query {
+  // These are the columns that the user selected, by fully-qualified property.
+  // It's a map from property name to FromColumnMeta.
+  private selectCols: Map<string, FromColumnMeta> = new Map();
+
+  // The order of the query.
+  private order: OrderByType[] = [];
+
+  /**
+   * Initialize the query using a From instance.
+   * @param colStore - Used for accessing columns in tables.
+   * @param tblStore - Used for accessing tables in the database.
+   * @param relStore - Used for accessing relationships between tables.
+   * @param propStore - Used for pulling table property maps (used in
+   * conjunction with the relStore to get remote columns).
+   * @param escaper - An [[Escaper]] matching the database type (e.g.
+   * [[MySQLEscaper]] or [[MSSQLEscaper]]).  Used when escaping column names in
+   * compiled conditions.
+   * @param executer - An [[Executer]] instance that matches the database type
+   * (e.g. [[MySQLExecuter]]).
+   * @param from - A [[From]] instance which holds the base table, all
+   * joined-in tables, and the where clause.
+   */
+  constructor(
+    protected colStore: ColumnStore,
+    protected tblStore: TableStore,
+    protected relStore: RelationshipStore,
+    protected propStore: PropertyMapStore,
+    protected escaper: Escaper,
+    protected executer: Executer,
+    private from: From) {
+
+    super(colStore, tblStore, relStore, propStore, escaper, executer);
+  }
+
+  /**
+   * Select columns manually.
+   * @param cols - An optional set of columns to select.  Each argument should
+   * be a fully-qualified property name in the form
+   * &lt;table-alias&gt;.&lt;property&gt;.  If no columns are specified, then
+   * all columns are selected.
+   */
+  select(...cols: string[]): Select<T> {
+    const fromMeta = this.from.getFromMeta();
+
+    // Select may only be performed once on a query.
+    assert(this.selectCols.size === 0,
+      'select already performed on query.');
+
+    // If no columns are provided, select all.
+    if (cols.length === 0) {
+      cols = this.from
+        .getFromMeta()
+        .getFromColumnMeta()
+        .map(col => col.fqProp);
+    }
+
+    cols.forEach(fqProp => {
+      // Each column can only be selected once.  This is only a constraint because
+      // of the way that the primary key is found in execute.  If the primary key
+      // of a table was selected twice, there would not be a way to serialize
+      // the primary key correctly.
+      assert(!this.selectCols.has(fqProp),
+        `Column "${fqProp}" already selected.`);
+
+      // Store the necessary metadata about the column selection.  This is
+      // what's needed for converting the query to a string, and for
+      // serialization.
+      // Throws if the column is not available for selection.
+      const fromColMeta = fromMeta.getFromColumnMetaByProp(fqProp);
+      this.selectCols.set(fqProp, fromColMeta);
+    });
+
+    // The primary key from each table must be selected.  The serialization
+    // needs a way to uniquely identify each object; the primary key is used
+    // for this.
+    const fromTblMetas = fromMeta.getFromTableMeta();
+
+    fromTblMetas
+      .forEach(fromTblMeta => {
+        const tblAlias = fromTblMeta.alias;
+        const tblMeta  = fromTblMeta.tableMetadata;
+
+        // This is the primary key of the table, which is an array.
+        const pk = this.colStore
+          .getPrimaryKey(tblMeta.Entity);
+
+        for (let i = 0; i < pk.length; ++i) {
+          // This is the alias of the column in the standard
+          // <table-alias>.<property> format.
+          const pkAlias = ColumnMetadata
+            .createFQName(tblAlias, pk[i].mapTo);
+
+          assert(this.selectCols.has(pkAlias),
+            'The primary key of every table must be selected, but the primary key ' +
+            `of table "${tblMeta.name}" (alias "${tblAlias}") ` +
+            'was not selected.');
+        }
+      });
+
+    return this;
+  }
+
+  /**
+   * Order by one or more columns.
+   * @param orders - A list of fully-qualified properties in the form
+   * &lt;table-alias&gt;.&lt;property&gt;, or an array of [[OrderByType]] with
+   * the fully-qualified property and direction.
+   */
+  orderBy(...orders: OrderByType[] | string[]): Select<T> {
+    // orderBy may only be called once.
+    assert(this.order.length === 0,
+      'orderBy already performed on query.');
+
+    for (let i = 0; i < orders.length; ++i) {
+      const order: OrderByType = typeof orders[i] === 'string' ?
+        {fqProperty: orders[i], dir: 'ASC'} as OrderByType :
+        orders[i] as OrderByType;
+
+      // Make sure the column is available for ordering.
+      const fromMeta = this.from.getFromMeta();
+
+      assert(fromMeta.isColumnAvailable(order.fqProperty),
+        `"${order.fqProperty}" is not available for orderBy.`);
+
+      this.order.push(order);
+    }
+
+    return this;
+  }
+
+  /**
+   * Get the SQL that represents the query.
+   * @return The SQL representing the select statement.
+   */
+  toString(): string {
+    let sql = 'SELECT  ';
+
+    // No columns specified.
+    assert(this.selectCols.size, 'No columns selected.  Call select().');
+
+    // Escape each selected column and add it to the query.
+    const cols = Array.from(this.selectCols.values());
+
+    sql += cols
+      .map(col => {
+        // Fully-qualified column name, escaped.
+        const colName = this.escaper.escapeFullyQualifiedColumn(col.fqColName);
+
+        // Alias is the fully-qualified property name, which is guaranteed unique
+        // even if the same table is referenced twice and makes it readable when
+        // debugging in that it gives information about the mapping.
+        const colAlias = this.escaper.escapeProperty(col.fqProp);
+
+        return `${colName} AS ${colAlias}`;
+      })
+      .join(',\n        ');
+
+    // Add the FROM (which includes the JOINS and WHERE).
+    sql += '\n';
+    sql += this.from.toString();
+
+    // Add the order.
+    if (this.order.length) {
+      const fromMeta = this.from.getFromMeta();
+
+      sql += '\n';
+      sql += 'ORDER BY ';
+
+      sql += this.order
+        .map(order => {
+          const colMeta = fromMeta.getFromColumnMetaByProp(order.fqProperty);
+          const colName = this.escaper.escapeFullyQualifiedColumn(colMeta.fqColName);
+
+          return `${colName} ${order.dir}`;
+        })
+        .join(', ');
+    }
+
+    return sql;
+  }
+
+  /**
+   * Execute the query and return an array of results of type T.
+   * @return A promise that shall be resolved with the normalized query results
+   * of type T.  If an error occurs while executing the query, the returned
+   * promise shall be rejected with the unmodified error.
+   */
+  execute(): Promise<T[]> {
+    // Base-level schema (e.g. the FROM table).
+    let baseSchema: Schema;
+
+    // Schema lookup by table alias.
+    const schemaLookup: Map<string, Schema> = new Map();
+
+    // No columns specified.
+    assert(this.selectCols.size, 'No columns selected.  Call select().');
+
+    // The primary key for each table is needed to create each schema.  Find
+    // each primary key and create the schema.
+    const fromMeta     = this.from.getFromMeta();
+    const fromTblMetas = fromMeta.getFromTableMeta();
+
+    fromTblMetas
+      .forEach(fromTblMeta => {
+        const pk = this.colStore.getPrimaryKey(fromTblMeta.tableMetadata.Entity);
+
+        // TODO: Composite keys are not yet implemented.
+        assert(pk.length === 1, 'Composite keys are not currently supported.');
+
+        // Create the schema.  In the query, the PK column name will be the
+        // fully-qualified property.
+        const fqProp  = ColumnMetadata.createFQName(fromTblMeta.alias, pk[0].mapTo);
+        const colMeta = this.selectCols.get(fqProp);
+        const schema  = new Schema(fromTblMeta.tableMetadata, colMeta.columnMetadata, fqProp);
+
+        // Keep a lookup of table alias->schema.
+        schemaLookup.set(fromTblMeta.alias, schema);
+        
+        // If this table has no parent then the schema is top level.  Else
+        // this is a sub schema and the parent is guaranteed to be present in
+        // the lookup.
+        if (fromTblMeta.parentAlias === null)
+          baseSchema = schema;
+        else {
+          schemaLookup
+            .get(fromTblMeta.parentAlias)
+            .addSchema(schema, fromTblMeta.relationshipMetadata);
+        }
+      });
+
+    // Add each column/property to its schema.
+    this.selectCols.forEach(fromColMeta => {
+      // PK already present.
+      if (!fromColMeta.columnMetadata.isPrimary) {
+        schemaLookup
+          .get(fromColMeta.tableAlias)
+          .addColumn(fromColMeta.columnMetadata, fromColMeta.fqProp);
+      }
+    });
+
+    // Execute the query and map the results.
+    const exe = this.buildQuery();
+
+    return this.executer
+      .select(exe.query, exe.params)
+      .then(result => new DataMapper()
+        .serialize<T>(result, baseSchema));
+  }
+
+  /**
+   * Build the query.
+   * @return The string-representation of the query to execute along with query
+   * parameters.
+   */
+  buildQuery(): ExecutableQuery {
+    return {
+      query: this.toString(),
+      params: this.from.getFromMeta().paramList.params
+    };
+  }
+}
+

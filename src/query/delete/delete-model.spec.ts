41a858d11db25eb299932eaa3e0862e1e91987d7
Ben Botto <benjamin.botto@gmail.com>
2018-12-09 19:35:23 -0800

    Implementes DeleteModel.

---
+++ src/query/delete/delete-model.spec.ts
@@ -0,0 +1,108 @@
+import { Connection } from 'mysql2/promise';
+
+import metaFactory from '../../metadata/metadata-factory';
+
+import { initDB } from '../../test/entity/database';
+
+import { RelationshipStore } from '../../metadata/relationship/relationship-store';
+import { TableStore } from '../../metadata/table/table-store';
+import { ColumnStore } from '../../metadata/column/column-store';
+import { PropertyMapStore } from '../../metadata/property/property-map-store';
+import { EntityType } from '../../metadata/table/entity-type';
+
+import { MySQLEscaper } from '../escaper/mysql-escaper';
+import { MySQLExecuter } from '../executer/mysql-executer';
+
+import { User } from '../../test/entity/user.entity';
+import { DeleteModel } from './delete-model';
+
+describe('DeleteModel()', () => {
+  let relStore: RelationshipStore;
+  let tblStore: TableStore;
+  let colStore: ColumnStore;
+  let propStore: PropertyMapStore;
+  let escaper: MySQLEscaper;
+  let executer: MySQLExecuter;
+  let con: jasmine.SpyObj<Connection>;
+  let getDelete: <T>(Entity: EntityType, model: T) => DeleteModel<T>;
+
+  beforeEach(() => {
+    initDB();
+
+    tblStore  = metaFactory.getTableStore();
+    colStore  = metaFactory.getColumnStore();
+    relStore  = metaFactory.getRelationshipStore();
+    propStore = metaFactory.getPropertyMapStore();
+    escaper   = new MySQLEscaper();
+    con       = jasmine.createSpyObj('con', ['query']);
+    executer  = new MySQLExecuter(con);
+
+    // Get a DeleteModel instance using an entity type and entity.
+    getDelete = <T>(Entity: EntityType, model: T) =>
+      new DeleteModel(colStore, tblStore, relStore, propStore, escaper, executer, Entity, model);
+  });
+
+  describe('.toString()', () => {
+    it('throws an error if the primary key is not set.', () => {
+      const user = new User();
+
+      expect(() => getDelete(User, user))
+        .toThrowError('The primary key is required when mutating a model, but "id" is missing.');
+    });
+
+    it('returns the correct delete statement for a model', () => {
+      const user = new User();
+      user.id = 42;
+
+      const del = getDelete(User, user);
+
+      expect(del.toString()).toBe(
+        'DELETE  `users`\n' +
+        'FROM    `users` AS `users`\n' +
+        'WHERE   (`users`.`userID` = :users_id_0)');
+    });
+  });
+
+  describe('.execute()', () => {
+    let deleteSpy: jasmine.Spy;
+
+    beforeEach(() => {
+      deleteSpy = spyOn(executer, 'delete');
+      deleteSpy.and.returnValue(Promise.resolve({affectedRows: 1}));
+    });
+
+    it('uses the Executer.delete method to execute the query.', (done) => {
+      const user = new User();
+      user.id = 42;
+
+      const del = getDelete(User, user);
+
+      del
+        .execute()
+        .then(u => {
+          expect(u).toBe(user);
+          done();
+        });
+
+      expect(deleteSpy.calls.argsFor(0)[0]).toBe(del.toString());
+      expect(deleteSpy.calls.argsFor(0)[1]).toEqual({users_id_0: 42});
+    });
+
+    it('rejects with an error if no rows are affected.', (done) => {
+      deleteSpy.and.returnValue(Promise.resolve({affectedRows: 0}));
+
+      const user = new User();
+      user.id = 42;
+      user.first = 'Ben';
+      user.last = 'Botto';
+
+      getDelete(User, user)
+        .execute()
+        .catch(err => {
+          expect(err.message).toBe('Delete operation did not affect any rows.');
+          done();
+        });
+    });
+  });
+});
+

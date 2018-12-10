11cdd83a93edf159399e0c3f01a6bd157c9dae5b
Ben Botto <benjamin.botto@gmail.com>
2018-12-09 19:05:57 -0800

    Implements Delete.

---
+++ src/query/delete/delete.spec.ts
@@ -0,0 +1,134 @@
+import { Connection } from 'mysql2/promise';
+
+import metaFactory from '../../metadata/metadata-factory';
+
+import { initDB } from '../../test/entity/database';
+
+import { RelationshipStore } from '../../metadata/relationship/relationship-store';
+import { TableStore } from '../../metadata/table/table-store';
+import { ColumnStore } from '../../metadata/column/column-store';
+import { PropertyMapStore } from '../../metadata/property/property-map-store';
+import { EntityType } from '../../metadata/table/entity-type';
+
+import { MySQLEscaper } from '../escaper/mysql-escaper';
+import { MySQLExecuter } from '../executer/mysql-executer';
+
+import { From } from '../from/from';
+import { Query } from '../query';
+import { Converter } from '../../converter/converter';
+import { UCConverter } from '../../test/converter/uc-converter';
+import { Delete } from './delete';
+
+import { User } from '../../test/entity/user.entity';
+import { PhoneNumber } from '../../test/entity/phone-number.entity';
+
+describe('Delete()', () => {
+  let relStore: RelationshipStore;
+  let tblStore: TableStore;
+  let colStore: ColumnStore;
+  let propStore: PropertyMapStore;
+  let escaper: MySQLEscaper;
+  let executer: MySQLExecuter;
+  let con: jasmine.SpyObj<Connection>;
+  let getFrom: (FromEntity: EntityType, fromAlias?: string) => From;
+  let getDelete: (from: From, alias?: string) => Delete;
+
+  beforeEach(() => {
+    initDB();
+
+    tblStore  = metaFactory.getTableStore();
+    colStore  = metaFactory.getColumnStore();
+    relStore  = metaFactory.getRelationshipStore();
+    propStore = metaFactory.getPropertyMapStore();
+    escaper   = new MySQLEscaper();
+    con       = jasmine.createSpyObj('con', ['query']);
+    executer  = new MySQLExecuter(con);
+
+    // From curry: produce a From instance with just an entity and alias.
+    getFrom = (FromEntity: EntityType, fromAlias?: string) =>
+      new From(colStore, tblStore, relStore, propStore, escaper, FromEntity, fromAlias);
+
+    getDelete = (from: From, alias?: string) =>
+      new Delete(colStore, tblStore, relStore, propStore, escaper, executer, from, alias);
+  });
+
+  describe('.constructor()', () => {
+    it('extends Query.', () => {
+      const del = getDelete(
+        getFrom(User, 'u'));
+
+      expect(del instanceof Query).toBe(true);
+    });
+  });
+
+  describe('.toString()', () => {
+    it('uses the alias from the base table if none is provided.', () => {
+      const del = getDelete(
+        getFrom(User, 'u')
+          .where({$eq: {'u.id': ':userID'}}, {userID: 42}));
+
+      expect(del.toString()).toBe(
+        'DELETE  `u`\n' +
+        'FROM    `users` AS `u`\n' +
+        'WHERE   `u`.`userID` = :userID'
+      );
+    });
+
+    it('uses the provided alias.', () => {
+      const from = getFrom(User, 'u')
+        .innerJoin(PhoneNumber, 'pn', 'u.phoneNumbers')
+        .where({$eq: {'u.id': ':userID'}}, {userID: 42});
+      const del = getDelete(from, 'pn');
+
+      expect(del.toString()).toBe(
+        'DELETE  `pn`\n' +
+        'FROM    `users` AS `u`\n' +
+        'INNER JOIN `phone_numbers` AS `pn` ON `u`.`userID` = `pn`.`userID`\n' +
+        'WHERE   `u`.`userID` = :userID'
+      );
+    });
+  });
+
+  describe('.execute()', () => {
+    let deleteSpy: jasmine.Spy;
+
+    beforeEach(() => {
+      deleteSpy = spyOn(executer, 'delete');
+      deleteSpy.and.returnValue(Promise.resolve({affectedRows: 1}));
+    });
+
+    it('executes the query using the Executer.delete method.', (done) => {
+      const del = getDelete(
+        getFrom(User, 'u')
+          .where({$eq: {'u.id': ':userID'}}, {userID: 42}));
+
+      del
+        .execute()
+        .then(res => {
+          expect(res.affectedRows).toBe(1);
+          done();
+        });
+
+      expect(deleteSpy).toHaveBeenCalled();
+      expect(deleteSpy.calls.argsFor(0)[0]).toBe(del.toString());
+      expect(deleteSpy.calls.argsFor(0)[1]).toEqual({userID: 42});
+    });
+
+    it('propagates errors from the Executer.delete() method.', (done) => {
+      const del = getDelete(
+        getFrom(User, 'u')
+          .where({$eq: {'u.id': ':userID'}}, {userID: 42}));
+      const err = new Error();
+
+      deleteSpy.and.returnValue(Promise.reject(err));
+
+      del
+        .execute()
+        .catch(e => {
+          expect(e).toBe(err);
+          done();
+        });
+    });
+  });
+});
+
---
+++ src/query/delete/delete.ts
@@ -0,0 +1,91 @@
+import { assert } from '../../error/assert';
+
+import { ColumnStore } from '../../metadata/column/column-store';
+import { TableStore } from '../../metadata/table/table-store';
+import { RelationshipStore } from '../../metadata/relationship/relationship-store';
+import { PropertyMapStore } from '../../metadata/property/property-map-store';
+
+import { Query } from '../query';
+import { Escaper } from '../escaper/escaper';
+import { Executer } from '../executer/executer';
+import { From } from '../from/from';
+import { FromColumnMeta } from '../from/from-column-meta';
+import { ExecutableQuery } from '../executable-query';
+import { MutateResultType } from '../executer/mutate-result-type';
+
+/**
+ * A [[Query]] class that represents a DELETE query.
+ */
+export class Delete extends Query {
+  /**
+   * Initialize the [[Query]] using a [[From]] instance.
+   * @param colStore - Used for accessing columns in tables.
+   * @param tblStore - Used for accessing tables in the database.
+   * @param relStore - Used for accessing relationships between tables.
+   * @param propStore - Used for pulling table property maps (used in
+   * conjunction with the relStore to get remote columns).
+   * @param escaper - An [[Escaper]] matching the database type (e.g.
+   * [[MySQLEscaper]] or [[MSSQLEscaper]]).  Used when escaping column names in
+   * compiled conditions.
+   * @param executer - An [[Executer]] instance that matches the database type
+   * (e.g. [[MySQLExecuter]]).
+   * @param from - A [[From]] instance which holds the base table, all
+   * joined-in tables, and the where clause.
+   * @param alias - Alias of the table to delete from.  Optional and defaults
+   * to the base table.
+   */
+  constructor(
+    protected colStore: ColumnStore,
+    protected tblStore: TableStore,
+    protected relStore: RelationshipStore,
+    protected propStore: PropertyMapStore,
+    protected escaper: Escaper,
+    protected executer: Executer,
+    protected from: From,
+    protected alias?: string) {
+
+    super(colStore, tblStore, relStore, propStore, escaper, executer);
+
+    // Alias defaults to the base table.
+    if (!this.alias)
+      this.alias = this.from.getBaseTableMeta().alias;
+  }
+
+  /**
+   * Get the SQL that represents the query.
+   * @return The SQL representing the update statement.
+   */
+  toString(): string {
+    let sql = `DELETE  ${this.escaper.escapeProperty(this.alias)}\n`;
+
+    sql += this.from.toString();
+
+    return sql;
+  }
+
+  /**
+   * Build the query.
+   * @return The string-representation of the query to execute along with query
+   * parameters.
+   */
+  buildQuery(): ExecutableQuery {
+    return {
+      query: this.toString(),
+      params: this.from.getParameterList().getParams()
+    };
+  }
+
+  /**
+   * Execute the query.
+   * @return A promise that shall be resolved with an object containing an
+   * "affectedRows" property.  If an error occurs when executing the query, the
+   * returned promise shall be rejected with the error (unmodified).
+   */
+  execute(): Promise<MutateResultType> {
+    const exe = this.buildQuery();
+
+    return this.executer
+      .delete(exe.query, exe.params);
+  }
+}
+

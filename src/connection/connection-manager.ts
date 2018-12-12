51f1bf4317398f9c42e9725077c1ecded81e2e9b
Ben Botto <benjamin.botto@gmail.com>
2018-12-11 16:56:36 -0800

    Updates end() to return a promise.  Fixes doc on connect.  Adds generic type for underlying connectin type.

--- src/connection/connection-manager.ts
+++ src/connection/connection-manager.ts
@@ -4,23 +4,30 @@ import { ConnectionStateType } from './connection-state-type';
 /**
  * Manages database connections.
  */
-export abstract class ConnectionManager {
+export abstract class ConnectionManager<T> {
   /**
    * Connect to the database.
    * @param connOpts - Connection options for setting up a connection to the
    * database.
-   * @return A promise that will be resolved with this [[DataContext]] instance.
+   * @return A promise that will be resolved with this underlying connection
+   * instance.
    */
-  abstract connect(connOpts: ConnectionOptions): Promise<this>;
+  abstract connect(connOpts: ConnectionOptions): Promise<T>;
 
   /**
    * End the connection pool.
    */
-  abstract end(): void;
+  abstract end(): Promise<void>;
 
   /**
    * Get the connection state.
    */
   abstract getConnectionState(): ConnectionStateType;
+
+  /**
+   * Get the underlying connection object.  Its type is database flavor
+   * specific.
+   */
+   abstract getConnection(): T;
 }
 

85b25423d992abd4105a8c7249515e9f2956f19a
Ben Botto <benjamin.botto@gmail.com>
2019-04-05 14:12:45 -0700

    Util exports.

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,8 @@
+export * from './key-val-type';
+export * from './validator/sub-model-type-validator';
 export * from './validator/model-validator';
 export * from './validator/insert-model-validator';
 export * from './validator/update-model-validator';
 export * from './validator/delete-model-validator';
+export * from './transformer/model-transformer';
 

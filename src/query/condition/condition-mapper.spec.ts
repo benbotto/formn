af0ccf1b6982c65338c3d28b7f67dde0020c9018
Ben Botto <benjamin.botto@gmail.com>
2019-10-09 11:46:14 -0700

    High-level helper class to map condition column names.

---
+++ src/query/condition/condition-mapper.spec.ts
@@ -0,0 +1,102 @@
+import { ColumnLookup } from '../../metadata/';
+
+import { ConditionMapper } from '../';
+
+describe('ConditionMapper()', () => {
+  const mapper = new ConditionMapper();
+
+  describe('.map()', () => {
+    let columnLookup: ColumnLookup;
+
+    beforeEach(() => {
+      columnLookup = new ColumnLookup();
+
+      columnLookup
+        .addColumn('description', 'j.description')
+        .addColumn('address', 'j.address')
+        .addColumn('status', 'j.status');
+    });
+
+    it('converts a single comparison.', () => {
+      const cond = {$eq: {'description': ':desc'}};
+      const newCond = mapper.map(cond, columnLookup);
+
+      expect(newCond).toEqual({$eq: {'j.description': ':desc'}});
+    });
+
+    it('converts null conditions.', () => {
+      const cond = {$is: {'description': null as any}};
+      const newCond = mapper.map(cond, columnLookup);
+
+      expect(newCond).toEqual({$is: {'j.description': null}});
+    });
+
+    it('converts parameterized null conditions.', () => {
+      const cond = {$is: {'description': ':desc'}};
+      const newCond = mapper.map(cond, columnLookup);
+
+      expect(newCond).toEqual({$is: {'j.description': ':desc'}});
+    });
+
+    it('converts in conditions.', () => {
+      const cond = {$in: {'status': [':s0', ':s1']}};
+      const newCond = mapper.map(cond, columnLookup);
+
+      expect(newCond).toEqual({$in: {'j.status': [':s0', ':s1']}});
+    });
+
+    it('converts boolean conditions.', () => {
+      const cond = {
+        $and: [
+          {$eq: {'address': ':addr'}},
+          {
+            $or: [
+              {$eq: {'status': 'Pending'}},
+              {$eq: {'status': 'Paid'}},
+            ]
+          }
+        ]
+      };
+
+      const newCond = mapper.map(cond, columnLookup);
+
+      expect(newCond).toEqual({
+        $and: [
+          {$eq: {'j.address': ':addr'}},
+          {
+            $or: [
+              {$eq: {'j.status': 'Pending'}},
+              {$eq: {'j.status': 'Paid'}},
+            ]
+          }
+        ]
+      });
+    });
+
+    it('throws an error if a parameter replacement is missing.', () => {
+      try {
+        const cond = {$eq: {'description': ':foo'}};
+        const params = {};
+
+        mapper.map(cond, columnLookup, params);
+        expect(true).toBe(false);
+      }
+      catch (err) {
+        expect(err.message).toBe('Replacement value for parameter "foo" not present.');
+      }
+    });
+
+    it('throws an error if the column is not present in the lookup.', () => {
+      try {
+        const cond = {$eq: {'foo': ':foo'}};
+        const params = {};
+
+        mapper.map(cond, columnLookup, params);
+        expect(true).toBe(false);
+      }
+      catch (err) {
+        expect(err.message).toBe('Property "foo" not found in ColumnLookup.');
+      }
+    });
+  });
+});
---
+++ src/query/condition/condition-mapper.ts
@@ -0,0 +1,99 @@
+import {
+  ConditionLexer, ConditionParser, ParseTree, LexerToken, ParameterType
+} from '../';
+
+import { ColumnLookup } from '../../metadata';
+
+import { ConditionError } from '../../error/';
+
+/**
+ * High-level helper class that is used to change the names of properties in a
+ * condition using a ColumnLookup instance.
+ */
+export class ConditionMapper {
+  /**
+   * Map the condition.
+   * @param condStr - The condition string to parse.  If condStr is an object
+   * it is converted to a string using JSON.stringify.
+   * @param columnLookup - A [[ColumnLookup]] instance used to change the
+   * properties (loosly "columns") in the condition.
+   * @param params - An optional object containing key-value pairs that are
+   * used to replace parameters in the query.  The compiler verifies that there
+   * is a replacement for every parameter, but does not perform the actual
+   * replacement.
+   * @return The same condition, but with the column/property names changed.
+   */
+  map(condStr: string | object, columnLookup: ColumnLookup, params?: ParameterType): object {
+    const tokens = new ConditionLexer()
+      .parse(condStr);
+    const parseTree = new ConditionParser()
+      .parse(tokens);
+
+    const cond = recompile(parseTree, columnLookup, params);
+
+    return cond;
+
+    // Recursively traverse the parse tree, changing any column names using the
+    // ColumnLookup, and rebuild the condition object.
+    function recompile(tree: ParseTree, columnLookup: ColumnLookup, params?: ParameterType): object {
+      switch (tree.token.type) {
+        case 'comparison-operator':
+        case 'null-comparison-operator': {
+          //   $eq
+          //  /   \
+          // col  val
+          const op     = tree.token.value;
+          const column = columnLookup.getColumn(tree.children[0].token.value as string);
+          const value  = getValue(tree.children[1].token, params);
+
+          return {[op]: {[column]: value}};
+        }
+
+        case 'in-comparison-operator': {
+          //   ___$in___
+          //  /    |    \
+          // col  val0  valN
+          const op     = tree.token.value;
+          const column = columnLookup.getColumn(tree.children[0].token.value as string);
+          const kids   = tree.children
+            .slice(1)
+            .map(kid => getValue(kid.token, params));
+
+          return {[op]: {[column]: kids}};
+        }
+
+        case 'boolean-operator': {
+          //   ___$and___
+          //  /    |     \
+          // cond0  cond1 condN
+          const op   = tree.token.value;
+          const kids = tree.children
+            .map(kid => recompile(kid, columnLookup));
+
+          return {[op]: kids};
+        }
+
+        default: {
+          // The only way this can fire is if the input parse tree did not come
+          // from the ConditionParser.  Trees from the ConditionParser are
+          // guaranteed to be syntactically correct.
+          throw new ConditionError(`Unknown type: ${tree.token.type}.`);
+        }
+      }
+
+      // Returns the value of token with a verification that it exists in params.
+      function getValue(token: LexerToken, params: ParameterType): string|number {
+        if (params && token.type === 'parameter') {
+          // Find the value in the params list (the leading colon is removed).
+          const paramKey = (token.value as string).substring(1);
+          const value    = params[paramKey];
+
+          if (value === undefined)
+            throw new ConditionError(`Replacement value for parameter "${paramKey}" not present.`);
+        }
+
+        return token.value;
+      }
+    }
+  }
+}

6153b9908c945fac8a44b7cffe5405c06a82bd99
Ben Botto <benjamin.botto@gmail.com>
2020-01-31 16:09:40 -0800

    Exports extended-user.

--- src/test/index.ts
+++ src/test/index.ts
@@ -8,6 +8,7 @@ export * from './entity/user-x-product.entity';
 export * from './entity/vehicle.entity';
 export * from './entity/vehicle-packages.entity';
 export * from './entity/type-test.entity';
+export * from './entity/extended-user.entity';
 export * from './util/to-plain';
 export * from './modelgenerator/model/people-model-table';
 export * from './modelgenerator/model/phone-numbers-model-table';

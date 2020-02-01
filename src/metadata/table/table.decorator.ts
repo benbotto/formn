0af3655a6f69855a5eb652783e729df837931064
Ben Botto <benjamin.botto@gmail.com>
2020-01-31 16:55:56 -0800

    Now supports entity extension by copying column, property, and relationship metadata from the parent entity.

--- src/metadata/table/table.decorator.ts
+++ src/metadata/table/table.decorator.ts
@@ -11,9 +11,58 @@ export function Table(options: TableMetaOptions = new TableMetaOptions()) {
     if (!options.name)
       options.name = ctor.name;
 
+    const tableStore = metaFactory
+      .getTableStore();
+
     metaFactory
       .getTableStore()
       .addTableMetadata(new TableMetadata(ctor, options.name, options.schema));
+
+    // If this table is an extension of another, then all the metadata (Column,
+    // Relationship, and PropertyMap) from the parent gets propagated to the
+    // child.
+    const parentCtor = Object.getPrototypeOf(ctor.prototype).constructor;
+    const colStore   = metaFactory.getColumnStore();
+
+    if (colStore.hasColumnMetadata(parentCtor)) {
+      const pColMetas = colStore.getColumnMetadata(parentCtor);
+
+      // Clone each ColumnMetadata for the parent table and swap out the
+      // Entity.
+      for (const pColMeta of pColMetas) {
+        const cColMeta = pColMeta.clone();
+
+        cColMeta.Entity = ctor;
+
+        colStore.addColumnMetadata(cColMeta);
+      }
+
+      // Copy the property map down to the child.
+      const propStore = metaFactory.getPropertyMapStore();
+      const propMap   = propStore.getPropertyMap(parentCtor);
+
+      for (const prop in propMap)
+        propStore.addProperty(ctor, prop);
+
+      // Copy over any relationships.  Note that only relationships on the
+      // parent are copied over.  E.g. if User has PhoneNumber[] and
+      // ExtendedUser extends User, the relationship from User to PhoneNumber
+      // is copied from User to ExtendedUser, but the relationship from
+      // PhoneNumber back to User is not copied.  1) Circular dependencies
+      // require runtime resolution, and 2) when mapping, the type of related
+      // instances would be incorrect (the parent class).
+      const relStore  = metaFactory.getRelationshipStore();
+      // Relationships from the parent to any table (one-way).
+      const pRelations = relStore.getRelationships(parentCtor, null, true);
+
+      for (const pRelation of pRelations) {
+        const cRelation = pRelation.clone();
+
+        cRelation.Entity = ctor;
+
+        relStore.addRelationshipMetadata(cRelation);
+      }
+    }
   }
 }
 
